7adc83f86ec1cb462b12f62eb4e3962bbab33983
Erick <ericklucio-suv@hotmail.com>
2020-06-29 23:39:44 -0300

    .

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -28,41 +28,8 @@ import img_boleto from '../../assets/imgs/img_boleto_icon.png';
       
       return (
         <>
-          <Container className="class_footer_root_container" fluid={true}>
-              <Row className="class_footer_row1">
-                  <Col md="12" xs="12" >
-                    <h3 className="class_footer_payment_methods_title">
-                      Formas de Pagamentos
-                    </h3>
-                  </Col>
-                  <Col  xs="4" sm="4" md="2" className="class_footer_payment_methods">
-                    <img src={img_visa_icon} className="class_footer_each_img_payment_method"></img>
-                 
-                  </Col>
-                  <Col  xs="4" sm="4" md="2" className="class_footer_payment_methods">
-                    <img src={img_mastercard_icon} className="class_footer_each_img_payment_method"></img>
-                   
-                  </Col>
-                  <Col xs="4" sm="4" md="2" className="class_footer_payment_methods">
-                    <img src={img_boleto} className="class_footer_each_img_payment_method"></img>
-                   
-                  </Col>
-                  
-                  
-              </Row>
-              <Row className="class_footer_row2">
-                <Col>
-                  <span className="class_footer_adress_tittle">
-                  Pedro Leopoldo MG CEP 33600000  Rua 
-                  </span>
-                   
-                </Col>
-              </Row>
-              <Row className="class_footer_row3">
-                <Col>
-                    Desenvolvido por <a href="https://dragonborndev.com/" target="_blank">Erick Dev</a> 2020
-                </Col>
-              </Row>
+          <Container >
+
           </Container>
         </>
       )
