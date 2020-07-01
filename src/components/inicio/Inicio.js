03645e0c7044be190eca4968dbf5b16dfcb7f8d7
Erick <ericklucio-suv@hotmail.com>
2020-07-01 12:16:25 -0300

    .

--- src/components/inicio/Inicio.js
+++ src/components/inicio/Inicio.js
@@ -15,7 +15,9 @@ import './Inicio.css';
 
 
 /*import imgs */
-
+import livroImg from '../../assets/imgs/img_livro_acenda.png';
+import camisetaImg from '../../assets/imgs/img_camisa_solidaria.png';
+import passaroImg from '../../assets/imgs/img_passaro.png';
 
 export default function Inicio(){
     window.scrollTo(0, 0); 
@@ -25,11 +27,19 @@ export default function Inicio(){
         <>           
             <Container className="class_inicio_container" fluid={true}>
                 <Row>
-                    <Col>
-                        <img>
-                        
-                        </img>
+                <Col sm="2">
+                </Col>
+                    <Col sm="8" className="class_inicio_col_1_womansmile_img">
+                        <div className="class_inicio_col_1_texts">
+                            <h3>Lorem Ipsum dolor sit amet</h3>
+                            <h4>Lorem Ipsum dolor sit amet</h4>
+                            <button className="class_inicio_col_1_button">LOREM IPSUM</button>
+                        </div>
+
+
                     </Col>
+                <Col sm="2">
+                </Col>
                 </Row>    
                 <Row>
                     <Col sm="2">
@@ -41,11 +51,23 @@ left
                     </Col>
                     <Col sm="8">
                         <Row>
-                            <Col sm="8">
-                            aaa
+                            <Col sm="8" className="class_inicio_col_viagem_img">
+                                
                             </Col>
                             <Col sm="4">
-                            bbbb
+                                <Row>
+                                    <Col className="class_inicio_col_woman_group_img">
+
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col className="class_inicio_col_woman_yoda">
+                                    
+                                    </Col>
+                                    <Col className="class_inicio_col_condicionador">
+                                    
+                                    </Col>
+                                </Row>
                             </Col>
                         </Row>
                     </Col>
@@ -58,27 +80,62 @@ left
                     </Col>
                 </Row>
                 <Row>
-                    <Col>
-                    1
+                    <Col sm="2"></Col>
+                    <Col className="class_inicio_col_itens">
+                        <Row>
+                            <Col className="class_inicio_col_especialists_sugest">
+                                <span>Nossos&nbsp;Especialistas <strong>Recomendam</strong></span>
+                            </Col>
+                            <Col className="class_inicio_col_itens_img">
+                                <ul>
+                                    <li><img src={livroImg} className="class_inicio_img_iten"></img></li>
+                                    <li>description</li>
+                                    <li>Carol Rache</li>
+                                    <li>4545,89</li>
+                                </ul>
+                            </Col>
+                            <Col className="class_inicio_col_itens_img">
+                                <ul>
+                                    <li><img src={camisetaImg} className="class_inicio_img_iten"></img></li>
+                                    <li>description</li>
+                                    <li>Tamanho Unico</li>
+                                    <li>4545,89</li>
+                                </ul>
+                            </Col>
+                            <Col className="class_inicio_col_itens_img">
+                                <ul>
+                                    <li><img src={passaroImg} className="class_inicio_img_iten"></img></li>
+                                    <li>description</li>
+                                    <li>4545,89</li>
+                                </ul>
+                            </Col>
+                            <Col className="class_inicio_col_itens_img">
+                                <ul>
+                                <li><img src={livroImg} className="class_inicio_img_iten"></img></li>
+                                    <li>description</li>
+                                    <li>Carol Rache</li>
+                                    <li>4545,89</li>
+                                </ul>
+                            </Col>
+                            <Col className="class_inicio_col_itens_img">
+                                <ul>
+                                <li><img src={camisetaImg} className="class_inicio_img_iten"></img></li>
+                                    <li>description</li>
+                                    <li>Tamanho Unico</li>
+                                    <li>4545,89</li>
+                                </ul>
+                            </Col>
+                            <Col className="class_inicio_col_itens_img">
+                                <ul>
+                                    <li><img src={passaroImg} className="class_inicio_img_iten"></img></li>
+                                    <li>description</li>
+                                    <li>4545,89</li>
+                                </ul>
+                            </Col>
+                        </Row>
                     </Col>
-                    <Col>
-                    2
-                    </Col>
-                    <Col>
-                    3
-                    </Col>
-                    <Col>
-                    4
-                    </Col>
-                    <Col>
-                    5
-                    </Col>
-                    <Col>
-                    6
-                    </Col>
-                    <Col>
-                    7
-                    </Col>
+                    <Col sm="2"></Col>
+
                 </Row>
             </Container>
             
